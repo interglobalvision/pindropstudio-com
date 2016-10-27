d7d1792c039e5aacd8ce7b6c9e48c04d077ab717
cas <cas@interglobal.vision>
2016-10-26 19:52:53 -0500

    add event post type

--- lib/post-types.php
+++ lib/post-types.php
@@ -3,13 +3,17 @@
 // https://developer.wordpress.org/resource/dashicons/
 function add_menu_icons_styles(){
 ?>
- 
+
 <style>
 #menu-posts-project .dashicons-admin-post:before {
-    content: '\f319';
+  content: '\f319';
+}
+
+#menu-posts-event .dashicons-admin-post:before {
+  content: '\f145';
 }
 </style>
- 
+
 <?php
 }
 add_action( 'admin_head', 'add_menu_icons_styles' );
@@ -20,41 +24,85 @@ add_action( 'init', 'register_cpt_project' );
 
 function register_cpt_project() {
 
-    $labels = array( 
-        'name' => _x( 'Projects', 'project' ),
-        'singular_name' => _x( 'Project', 'project' ),
-        'add_new' => _x( 'Add New', 'project' ),
-        'add_new_item' => _x( 'Add New Project', 'project' ),
-        'edit_item' => _x( 'Edit Project', 'project' ),
-        'new_item' => _x( 'New Project', 'project' ),
-        'view_item' => _x( 'View Project', 'project' ),
-        'search_items' => _x( 'Search Projects', 'project' ),
-        'not_found' => _x( 'No projects found', 'project' ),
-        'not_found_in_trash' => _x( 'No projects found in Trash', 'project' ),
-        'parent_item_colon' => _x( 'Parent Project:', 'project' ),
-        'menu_name' => _x( 'Projects', 'project' ),
-    );
+  $labels = array( 
+    'name' => _x( 'Projects', 'project' ),
+    'singular_name' => _x( 'Project', 'project' ),
+    'add_new' => _x( 'Add New', 'project' ),
+    'add_new_item' => _x( 'Add New Project', 'project' ),
+    'edit_item' => _x( 'Edit Project', 'project' ),
+    'new_item' => _x( 'New Project', 'project' ),
+    'view_item' => _x( 'View Project', 'project' ),
+    'search_items' => _x( 'Search Projects', 'project' ),
+    'not_found' => _x( 'No projects found', 'project' ),
+    'not_found_in_trash' => _x( 'No projects found in Trash', 'project' ),
+    'parent_item_colon' => _x( 'Parent Project:', 'project' ),
+    'menu_name' => _x( 'Projects', 'project' ),
+  );
 
-    $args = array( 
-        'labels' => $labels,
-        'hierarchical' => false,
-        
-        'supports' => array( 'title', 'editor', 'thumbnail' ),
-        
-        'public' => true,
-        'show_ui' => true,
-        'show_in_menu' => true,
-        'menu_position' => 5,
-        
-        'show_in_nav_menus' => true,
-        'publicly_queryable' => true,
-        'exclude_from_search' => false,
-        'has_archive' => true,
-        'query_var' => true,
-        'can_export' => true,
-        'rewrite' => true,
-        'capability_type' => 'post'
-    );
+  $args = array( 
+    'labels' => $labels,
+    'hierarchical' => false,
 
-    register_post_type( 'project', $args );
+    'supports' => array( 'title', 'editor', 'thumbnail' ),
+
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+
+    'show_in_nav_menus' => true,
+    'publicly_queryable' => true,
+    'exclude_from_search' => false,
+    'has_archive' => true,
+    'query_var' => true,
+    'can_export' => true,
+    'rewrite' => true,
+    'capability_type' => 'post'
+  );
+
+  register_post_type( 'project', $args );
+}
+
+// Register Custom Post: Event
+add_action( 'init', 'register_cpt_event' );
+
+function register_cpt_event() {
+
+  $labels = array( 
+    'name' => _x( 'Events', 'event' ),
+    'singular_name' => _x( 'Event', 'event' ),
+    'add_new' => _x( 'Add New', 'event' ),
+    'add_new_item' => _x( 'Add New Event', 'event' ),
+    'edit_item' => _x( 'Edit Event', 'event' ),
+    'new_item' => _x( 'New Event', 'event' ),
+    'view_item' => _x( 'View Event', 'event' ),
+    'search_items' => _x( 'Search Events', 'event' ),
+    'not_found' => _x( 'No events found', 'project' ),
+    'not_found_in_trash' => _x( 'No events found in Trash', 'project' ),
+    'parent_item_colon' => _x( 'Parent Event:', 'event' ),
+    'menu_name' => _x( 'Events', 'event' ),
+  );
+
+  $args = array( 
+    'labels' => $labels,
+    'hierarchical' => false,
+
+    'supports' => array( 'title', 'editor', 'thumbnail' ),
+
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+
+    'show_in_nav_menus' => true,
+    'publicly_queryable' => true,
+    'exclude_from_search' => false,
+    'has_archive' => true,
+    'query_var' => true,
+    'can_export' => true,
+    'rewrite' => true,
+    'capability_type' => 'post'
+  );
+
+  register_post_type( 'event', $args );
 }
