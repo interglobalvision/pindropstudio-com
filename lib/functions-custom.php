0a91cb8869703080f0fdd1619d924c60cc913b99
Patrick Best <patrickbest@patrickbest.com>
2016-11-04 15:18:34 -0600

    add render function

--- lib/functions-custom.php
+++ lib/functions-custom.php
@@ -50,3 +50,33 @@ function menuActiveClasses($item = null, $type = null, $classes = null) {
 function render_embed_caption($caption) {
   echo '<h5 class="text-align-center margin-top-tiny margin-bottom-tiny">' . $caption . '</h5>';
 }
+
+
+function render_quote($text = null, $person = null, $luminary = null) {
+  if ($text === null || $person === null) {
+    return false;
+  }
+?>
+<div class="grid-item item-s-1 offset-s-1">
+  <div id="quote-pin-holder">
+    <?php echo url_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/pindrop-pin.svg'); ?>
+  </div>
+</div>
+<div class="grid-item item-s-8">
+  <div class="quote-text">
+    <?php echo $text; ?>
+  </div>
+  <div class="quote-person">
+<?php
+  if ($luminary) {
+?>
+    <a href="<?php echo get_permalink($luminary); ?>"><?php echo $person; ?></a>
+<?php
+  } else {
+    echo $person;
+  }
+?>
+  </div>
+</div>
+<?php
+}
