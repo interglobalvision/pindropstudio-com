158952ce969f140eb06711ae8220220f8a141aa0
Patrick Best <patrickbest@patrickbest.com>
2016-11-04 14:46:03 -0600

    add render functions for dividers

--- lib/functions-render.php
+++ lib/functions-render.php
@@ -5,3 +5,28 @@
 function render_embed_caption($caption) {
   echo '<h5 class="text-align-center margin-top-tiny margin-bottom-tiny">' . $caption . '</h5>';
 }
+
+function render_divider() {
+?>
+<div class="grid-item item-s-12">
+  <div class="dotted-divider-full"></div>
+</div>
+<?php
+}
+
+function render_divider_with_content($content) {
+  if (!$content) {
+    render_divider();
+  }
+?>
+<div class="grid-item item-s-12 font-style-micro font-size-small text-align-center">
+  <div class="dotted-divider">
+    <div class="dotted-divider-side dotted-divider-left"></div>
+    <div class="dotted-divider-center">
+      <?php echo $content; ?>
+    </div>
+    <div class="dotted-divider-side dotted-divider-right"></div>
+  </div>
+</div>
+<?php
+}
