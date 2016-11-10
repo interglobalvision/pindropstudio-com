4f9168bf5de2bfdc0f1ba6b0ee14c2b872bdb3ed
cas <cas@interglobal.vision>
2016-11-10 17:49:30 -0600

    add checks for data in submit page

--- page-submit.php
+++ page-submit.php
@@ -15,31 +15,57 @@ if( have_posts() ) {
       foreach($open_submissions as $index => $submission) {
   ?>
   <article <?php post_class('container submission'); ?>>
+      <?php
+        if (!empty($submission['_igv_submission_image_id'])) {
+      ?>
     <div class="grid-row margin-top-basic margin-bottom-basic u-flex-center">
       <div class="grid-item item-s-6">
         <?php echo wp_get_attachment_image($submission['_igv_submission_image_id'], 'l-6'); ?>
       </div>
     </div>
+      <?php
+        }
+
+        if (!empty($submission['_igv_submission_title'])) {
+      ?>
     <div class="grid-row u-flex-center">
       <div class="grid-item item-s-9 margin-bottom-basic text-align-center">
         <h3><?php echo $submission['_igv_submission_title']; ?></h3>
       </div>
     </div>
+      <?php
+        }
+
+        if (!empty($submission['_igv_submission_desc'])) {
+      ?>
     <div class="grid-row u-flex-center">
       <div class="grid-item item-s-8 margin-bottom-basic">
         <?php echo apply_filters('the_content', $submission['_igv_submission_desc']); ?>
       </div>
     </div>
+      <?php
+        }
+
+        if (!empty($submission['_igv_submission_button_text'])) {
+      ?>
     <div class="grid-row u-flex-center">
       <div class="grid-item item-s-8 margin-bottom-basic">
       <div class="text-align-center"><a class="expandable-toggle no-hide link-button" data-exapandable-id="expandable-<?php echo $index; ?>"><?php echo $submission['_igv_submission_button_text']; ?></a></div>
       </div>
     </div>
+      <?php
+        }
+
+        if (!empty($submission['_igv_submission_form'])) {
+      ?>
     <div id="expandable-<?php echo $index; ?>" class="expandable-content grid-row u-flex-center">
       <div class="grid-item item-s-8 offset-s-2 margin-bottom-basic">
         <?php gravity_form($submission['_igv_submission_form']); ?>
       </div>
     </div>
+      <?php
+        }
+      ?>
   </article>
 <?php
       }
