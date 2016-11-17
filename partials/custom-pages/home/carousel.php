1d58f2f4a76af67b029b0951f2805e0b28f4c89c
Patrick Best <patrickbest@patrickbest.com>
2016-11-17 17:57:22 -0600

    add link and conditional header and style

--- partials/custom-pages/home/carousel.php
+++ partials/custom-pages/home/carousel.php
@@ -5,7 +5,7 @@
 ?>
 <div class="grid-row margin-bottom-large">
   <div class="grid-item item-s-12">
-    <div class="swiper-container home-carousel text-align-center font-size-h3 font-style-shadow">
+    <div class="swiper-container home-carousel text-align-center font-style-shadow">
       <div class="swiper-wrapper">
       <?php
         global $post;
@@ -21,14 +21,43 @@
               the_post_thumbnail('l-12-carousel');
             }
           ?>
-          <div class="carousel-content align-items-center">
-            <h1><?php
-              if (!empty($carousel_post['_igv_carousel_title_override'])) {
-                echo $carousel_post['_igv_carousel_title_override'];
-              } else {
-                the_title();
-              }
-            ?></h1>
+          <div class="carousel-content-holder align-items-center">
+            <div class="carousel-content">
+              <?php
+                $post_type = get_post_type($post);
+
+                if ($post_type === 'event') {
+                  $now = new \Moment\Moment('now', 'Europe/London');
+                  $tomorrow = $now->addDays(1);
+                  $midnight = $tomorrow->startOf('day');
+                  $midnight_timestamp = strtotime($midnight->format());
+
+                  $time_meta = get_post_meta($post, '_igv_event_datetime', true);
+                  $time_moment = new \Moment\Moment('@' . $time_meta);
+
+                  echo '<h4 class="font-style-micro margin-bottom-small">';
+
+                  if ($time_meta > $midnight_timestamp) {
+                    echo 'Live | ' . $time_moment->format('l j F') . ' | ' . $time_moment->format('H:i');
+                  } else {
+                    echo 'Sound & Vision';
+                  }
+
+                  echo '</h4>';
+
+                }
+              ?>
+              <h1><a href="<?php the_permalink(); ?>"><?php
+                if (!empty($carousel_post['_igv_carousel_title_override'])) {
+                  echo $carousel_post['_igv_carousel_title_override'];
+                } else {
+                  the_title();
+                }
+              ?></a></h1>
+              <ul class="carousel-links margin-top-small font-style-micro u-inline-list">
+                <li><a href="<?php the_permalink(); ?>">Read More</a></li>
+              </ul>
+            </div>
           </div>
         </div>
       <?php
@@ -36,7 +65,7 @@
       ?>
       </div>
       <div class="carousel-pagination align-items-center justify-center">
-        <div class="swiper-pagination"></div>
+        <div class="swiper-pagination font-size-h3"></div>
       </div>
     </div>
   </div>
