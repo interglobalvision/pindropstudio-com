230b6f8a19ef759fd25f2e755f27718525f0be91
Patrick Best <patrickbest@patrickbest.com>
2017-01-11 19:11:15 -0600

    put people archive layout into 2 shuffle sections

--- archive-people.php
+++ archive-people.php
@@ -4,10 +4,9 @@ get_header();
 
 <main id="main-content">
   <section id="posts" class="container">
-    <div class="shuffle-section">
-      <div class="stuffle-preloader"></div>
-      <div class="shuffle-container grid-row hidden">
-
+    <div class="grid-row margin-bottom-small">
+      <?php render_divider('People'); ?>
+    </div>
 <?php
   $people_core = new WP_Query(array(
     'post_type' => 'people',
@@ -41,21 +40,40 @@ get_header();
   ));
 
   if ($people_core->have_posts()) {
+?>
+    <div class="shuffle-section">
+      <div class="stuffle-preloader"></div>
+      <div class="shuffle-container grid-row hidden">
+<?php
     while ($people_core->have_posts()) {
       $people_core->the_post();
       get_template_part('partials/people/person');
     }
+?>
+      </div>
+    </div>
+<?php
   }
 
   if ($people_circle->have_posts()) {
+?>
+    <div class="grid-row margin-top-basic margin-bottom-small">
+      <?php render_divider('Pindrop Circle'); ?>
+    </div>
+    <div class="shuffle-section">
+      <div class="stuffle-preloader"></div>
+      <div class="shuffle-container grid-row hidden">
+<?php
     while ($people_circle->have_posts()) {
       $people_circle->the_post();
       get_template_part('partials/people/person');
     }
-  }
 ?>
       </div>
     </div>
+<?php
+  }
+?>
   </section>
 
 </main>
