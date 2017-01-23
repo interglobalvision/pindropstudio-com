c47748a2e5a38d665a99ca789f073eb58805b0ca
Patrick Best <patrickbest@patrickbest.com>
2017-01-23 14:31:21 -0600

    add readmore support to people partial

--- partials/people/person.php
+++ partials/people/person.php
@@ -1,4 +1,7 @@
-<?php $title = get_post_meta($post->ID, '_igv_title', true); ?>
+<?php
+  $title = get_post_meta($post->ID, '_igv_title', true);
+  $explode_content = explode( '<!--more-->', $post->post_content );
+?>
 <article <?php post_class('shuffle-item item-s-12 item-m-6 item-l-4'); ?> id="post-<?php the_ID(); ?>">
   <div class="card">
     <div>
@@ -13,6 +16,24 @@
       <?php } ?>
     </header>
 
-    <?php the_content(); ?>
+    <?php
+    if (sizeof($explode_content) > 1) {
+      $content_before = apply_filters( 'the_content', $explode_content[0] );
+      $content_after = apply_filters( 'the_content', $explode_content[1] );
+    ?>
+      <div class="expandable-post">
+        <div class="expandable-excerpt">
+          <?php echo $content_before; ?>
+        </div>
+          <p class="text-align-right"><a class="font-style-micro font-uppercase font-size-small u-pointer expandable-toggle" data-exapandable-id="expandable-<?php echo $post->ID; ?>">Read more</a></p>
+        <div id="expandable-<?php echo $post->ID; ?>" class="expandable-content">
+          <?php echo $content_after; ?>
+        </div>
+      </div>
+    <?php
+    } else {
+      the_content();
+    }
+    ?>
   </div>
 </article>
