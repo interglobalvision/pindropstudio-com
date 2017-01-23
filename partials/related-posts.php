02fbabd9503f8daba6e5b6858e2d91f0f06426a0
Patrick Best <patrickbest@patrickbest.com>
2017-01-23 17:03:05 -0600

    dont show related post if ID returns no $post

--- partials/related-posts.php
+++ partials/related-posts.php
@@ -11,20 +11,22 @@
     global $post;
     foreach($related_posts as $post_id) {
       $post = get_post($post_id);
-      setup_postdata($post);
-?>
-  <div class="grid-item item-s-12 item-m-4 margin-bottom-small text-align-center">
-    <div class="card">
-      <a href="<?php the_permalink(); ?>">
-        <?php the_post_thumbnail('l-4', array('class' => 'margin-bottom-tiny')); ?>
-        <h3 class="margin-bottom-small"><?php the_title(); ?></h3>
-      </a>
-      <a href="<?php the_permalink(); ?>" class="link-button font-uppercase" >Read More</a>
+      if ($post) {
+        setup_postdata($post);
+    ?>
+    <div class="grid-item item-s-12 item-m-4 margin-bottom-small text-align-center">
+      <div class="card">
+        <a href="<?php the_permalink(); ?>">
+          <?php the_post_thumbnail('l-4', array('class' => 'margin-bottom-tiny')); ?>
+          <h3 class="margin-bottom-small"><?php the_title(); ?></h3>
+        </a>
+        <a href="<?php the_permalink(); ?>" class="link-button font-uppercase" >Read More</a>
+      </div>
     </div>
-  </div>
-<?php
+    <?php
+        wp_reset_postdata();
+      }
     }
-    wp_reset_postdata();
 ?>
 </div>
 <?php
