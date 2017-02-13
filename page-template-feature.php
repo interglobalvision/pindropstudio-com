b2b5bde8421a300f35f83bdb0b10ab2880c2cb76
Patrick Best <patrickbest@patrickbest.com>
2017-02-13 15:10:34 -0600

    add shuffle post partial from collection page template

--- page-template-feature.php
+++ page-template-feature.php
@@ -42,28 +42,7 @@ if( have_posts() ) {
         if (get_post_type($post->ID) === 'event') {
           get_template_part('partials/custom-pages/event-media');
         } else {
-?>
-          <article <?php post_class('media-item shuffle-item item-s-12 item-m-6 item-l-4'); ?> id="post-<?php the_ID(); ?>">
-            <div class="card">
-              <div class="margin-bottom-small">
-                <?php the_post_thumbnail('l-4-full', array('class' => 'u-block')); ?>
-              </div>
-
-              <a href="<?php the_permalink(); ?>">
-                <h4 class="font-style-micro font-size-small margin-bottom-small text-align-center">
-                  <?php the_time('d F Y'); ?>
-                </h4>
-                <h3 class="margin-bottom-small text-align-center">
-                  <?php the_title(); ?>
-                </h3>
-              </a>
-
-              <div class="text-align-center">
-                <a class="link-button font-size-small" href="<?php the_permalink(); ?>">Read More</a>
-              </div>
-            </div>
-          </article>
-<?php
+          get_template_part('partials/shuffle/shuffle-post');
         }
         wp_reset_postdata();
       }
