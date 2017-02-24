913bbba27a6f3602abd718a92ec29520797b165f
Patrick Best <patrickbest@patrickbest.com>
2017-02-23 18:23:45 -0600

    set max width of post copy to 620px
    and move containing divs so this can work

--- page-about.php
+++ page-about.php
@@ -93,8 +93,10 @@ if( have_posts() ) {
     </div>
     <div id="about-drawer-about" class="about-page-drawer active">
       <div class="grid-row">
-        <div class="grid-item item-s-12 item-m-8 offset-m-2 margin-bottom-basic post-copy">
-          <?php the_content(); ?>
+        <div class="grid-item item-s-12 item-m-8 offset-m-2 margin-bottom-basic">
+          <div class="post-copy">
+            <?php the_content(); ?>
+          </div>
         </div>
       </div>
     </div>
--- page-template-feature.php
+++ page-template-feature.php
@@ -20,8 +20,10 @@ if( have_posts() ) {
       </div>
     </div>
     <div class="grid-row">
-      <div class="grid-item item-s-12 item-m-8 offset-m-2 margin-bottom-basic post-copy">
-        <?php the_content(); ?>
+      <div class="grid-item item-s-12 item-m-8 offset-m-2 margin-bottom-basic">
+        <div class="post-copy">
+          <?php the_content(); ?>
+        </div>
       </div>
     </div>
 
