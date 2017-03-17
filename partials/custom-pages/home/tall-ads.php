6cc8ba0ef95c5534979fe028857e103154616b66
Patrick Best <patrickbest@patrickbest.com>
2017-03-17 15:08:44 -0600

    fix selector

--- partials/custom-pages/home/tall-ads.php
+++ partials/custom-pages/home/tall-ads.php
@@ -15,7 +15,7 @@
   $ad2_link_external = IGV_get_option('_igv_home_options', '_igv_tall_ad2_link_external');
   $ad2_link_text = IGV_get_option('_igv_home_options', '_igv_tall_ad2_button');
 ?>
-<div class="grid-row margin-top-large margin-bottom-large js-match-height-group" data-match-height-targets='["tall-ad-title"]'>
+<div class="grid-row margin-top-large margin-bottom-large js-match-height-group" data-match-height-targets='["tall-ad-title", "tall-ad-subtitle"]'>
   <?php
     render_tall_ad($ad1_image, $ad1_text, $ad1_subtitle, $ad1_link_internal, $ad1_link_autoplay, $ad1_link_external, $ad1_link_text);
     render_tall_ad($ad2_image, $ad2_text, $ad2_subtitle, $ad2_link_internal, $ad2_link_autoplay, $ad2_link_external, $ad2_link_text);
