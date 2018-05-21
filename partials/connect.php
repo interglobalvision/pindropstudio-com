9013b633c640f501968934cfd7e9fd4d33ed1d50
MattAppleton <matt@modernactivity.com>
2018-05-21 21:00:23 +0100

    Update connect.php
    renewed cm form, reCaptcha compatible now...

--- partials/connect.php
+++ partials/connect.php
@@ -2,24 +2,26 @@
   <?php render_divider('Sign up to our newsletter'); ?>
 </div>
 
-<form action="http://modernactivity.createsend.com/t/y/s/cjrdlh/" method="post" class="connect-form">
+<form id="captcha-form" action="https://modernactivity.createsend.com/t/y/s/sulhjr/" action="https://www.createsend.com/t/subscribeerror?description=" method="post" class="js-cm-form connect-form" data-id="191722FC90141D02184CB1B62AB3DC262F95B3347CC1ABDEF250925282703F163395E69468AD5F876CF8A8F96F467779A1A001B7E165D7A1C2E1654A34F6CA93">
   <div class="grid-row margin-bottom-mid">
     <div class="grid-item item-s-12 item-m-3 offset-m-1">
-      <input id="fieldName" name="cm-name" type="text" placeholder="First name..." />
+      <input id="fieldName" name="cm-name" type="text" placeholder="First name..." required />
     </div>
     <div class="grid-item item-s-12 item-m-3">
-      <input id="fieldcktlkk" name="cm-f-cktlkk" type="text" placeholder="Last Name..." />
+      <input id="fieldetltrj" name="cm-f-sduwd" type="text" placeholder="Last name..." />
     </div>
     <div class="grid-item item-s-12 item-m-3">
-      <input id="fieldEmail" name="cm-cjrdlh-cjrdlh" type="email" placeholder="Email address..." required="" />
+      <input id="fieldEmail" name="cm-gdhtyl-gdhtyl" class="js-cm-email-input" type="email" placeholder="Email address..." required />
     </div>
     <div class="grid-item item-s-12 item-m-1 text-align-center">
-      <button type="submit" class="connect-form-submit">
+     <button type="submit" class="connect-form-submit js-cm-submit-button">
         <?php echo url_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/pindrop-arrow.svg'); ?>
       </button>
     </div>
   </div>
 </form>
+
+<script type="text/javascript" src="https://js.createsend1.com/javascript/copypastesubscribeformlogic.js"></script>
 
 <?php
   $twitter = IGV_get_option('_igv_site_options', '_igv_socialmedia_twitter');
@@ -42,3 +44,29 @@
 <div class="grid-row margin-bottom-basic">
   <?php render_divider($social_divider_content); ?>
 </div>
+
+
+
+
+
+<!--
+<form id="subForm" class="js-cm-form" action="https://www.createsend.com/t/subscribeerror?description=" method="post" data-id="191722FC90141D02184CB1B62AB3DC262F95B3347CC1ABDEF250925282703F163395E69468AD5F876CF8A8F96F467779A1A001B7E165D7A1C2E1654A34F6CA93">
+    
+    <p>
+        <label for="fieldName">Name</label><br />
+        <input id="fieldName" name="cm-name" type="text" />
+    </p>
+    <p>
+        <label for="fieldEmail">Email</label><br />
+        <input id="fieldEmail" class="js-cm-email-input" name="cm-gdhtyl-gdhtyl" type="email" required /> 
+    </p>
+    <p>
+        <label for="fieldsduwd">Lastname</label><br />
+        <input id="fieldsduwd" name="cm-f-sduwd" type="text" />
+    </p>
+    <p>
+        <button class="js-cm-submit-button" type="submit">Subscribe</button> 
+    </p>
+</form>
+    <script type="text/javascript" src="https://js.createsend1.com/javascript/copypastesubscribeformlogic.js"></script>
+-->
